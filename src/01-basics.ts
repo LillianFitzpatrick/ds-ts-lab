82ddca7dea627f8ff5d14220e322ae2d3cb90829
lillianfitzpatrick <20102189@mail.wit.ie>
2026-09-21 13:09:14 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -5,24 +5,31 @@ interface Friend {
     phone: string;
     age: number
 }
-
-const friend1 = {
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number
+    } 
+  }
+const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
   age: 25,
 };
 
-const friend2 = {
+const friend2: Friend = {
   name: "Jane Costello",
   phone: "086--12345",
   age: 31,
 };
 
-const friends = [friend1, friend2];
+const friends : Friend[]= [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -31,7 +38,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2 : Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -40,7 +47,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3 : Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
